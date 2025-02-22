9c19e98ba0a64efed0ba66a821c5bb36853e7326
ysk0423 <ssyhrsysk23@gmail.com>
2025-02-22 18:10:53 +0900

    apprunner作成

---
+++ apprunner.tf
@@ -0,0 +1,18 @@
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/apprunner_service
+resource "aws_apprunner_service" "nextjs-express-runner" {
+  service_name = local.project_name
+  source_configuration {
+    image_repository {
+      image_identifier = aws_ecr_repository.nextjs_express.repository_url
+      image_repository_type = "ECR"
+      image_configuration {
+        port = 3000
+      }
+    }
+    auto_deployments_enabled = false
+  }
+  instance_configuration {
+    cpu = 256
+    memory = 512
+  }
+}
